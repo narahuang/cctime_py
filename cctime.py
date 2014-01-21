dd2a69dd28c406ab0f2d4ffd87fc7192a67305a3
Nara Huang <narahuang@gmail.com>
2014-01-21 14:25:23 +0800

    Modify comment in cctime.py

--- cctime.py
+++ cctime.py
@@ -1,5 +1,5 @@
 import datetime,time
-# This script calculates the number of AP from now to 23:00 CST
+# This script calculates the number of AP recovered from now to 23:00 CST
 # Nara Huang
 now = int(time.strftime("%H"))*60 + int(time.strftime("%M"))
 cct = 1380 - now
