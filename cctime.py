d5518862d615c10db41af27a7d24d895e06a2c7d
Nara Huang <narahuang@gmail.com>
2014-01-23 14:22:54 +0800

    Simplify the code

--- cctime.py
+++ cctime.py
@@ -1,6 +1,8 @@
 import datetime,time
 # This script calculates the number of AP recovered from now to 23:00 CST
 # Nara Huang
-now = int(time.strftime("%H"))*60 + int(time.strftime("%M"))
-cct = 1380 - now
-print cct/8
+#now = int(time.strftime("%H"))*60 + int(time.strftime("%M"))
+#cct = 1380 - now
+#print cct/8
+
+print ( 1380 - (int(time.strftime("%H"))*60 + int(time.strftime("%M"))) )/8
